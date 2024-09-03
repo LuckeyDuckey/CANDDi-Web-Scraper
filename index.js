e0d756df2a81a9b6793158488e56f1bb1cb5e4d6
LuckeyDuckey <charlieashtond@gmail.com>
2024-09-03 23:08:35 +0100

    Gets information from html pages and explores internal link to a certain depth to avoid traverling to far.

--- index.js
+++ index.js
@@ -1,53 +1,127 @@
-const readline = require("readline");
+const ReadLine = require("readline");
 const https = require("https");
+const Cheerio = require("cheerio");
+const Knwl = require("knwl.js");
 
-// Function to fetch HTML from URL
+let Emails = [];
+let PhoneNumbers = [];
+let Addresses = [];
+let ExploredPages = [];
+
+function Delay(MilliSeconds)
+{
+    return new Promise(Resolve => setTimeout(Resolve, MilliSeconds));
+}
+
+// Add item to an array while avoiding duplicates
+function AppendToArray(Item, Array)
+{
+    if (!Array.includes(Item)) { Array.push(Item); }
+}
+
+// Function to fetch raw HTML as string from URL
 function GetHTMLFromURL(URL)
 {
-    https.get(URL, (response) => {
+    return new Promise((Resolve, Reject) => {
+        https.get(URL, (Response) => {
 
-        let HTML = "";
-        response.on("data", (DataChunk) => {HTML += DataChunk});
-        response.on("end", () => {console.log(HTML)});
+            let HTML = "";
+            Response.on("data", (DataChunk) => { HTML += DataChunk; });
+            Response.on("end", () => { Resolve(HTML); });
 
-    }).on("error", () => {return null});
+        }).on("error", (Error) => { Reject(Error); });
+    });
 }
 
-function ExtractInformationFromHTML(RawHTML)
+function ExtractInformationFromHTML(HTML)
 {
-    null;
+    // Extract emails, phone numbers and addresses using Knwl.js
+    const KnwlInstance = new Knwl("english");
+    KnwlInstance.init(HTML);
+
+    const ExtractedEmails = KnwlInstance.get("emails");
+    ExtractedEmails.forEach((Email) => { AppendToArray(Email.address, Emails); });
+
+    const ExtractedPhones = KnwlInstance.get("phones");
+    ExtractedPhones.forEach((Number) => { AppendToArray(Number.phone, PhoneNumbers); });
+
+    const ExtractedAddresses = KnwlInstance.get("places");
+    ExtractedAddresses.forEach((Address) => { AppendToArray(Address.place, Addresses); });
 }
 
-async function ProcessPages(URLs)
+function ExtractInteralLinkFromHtml(HTML, BaseURL)
 {
-    // Loop over each page
+    // Extract internal links to further traverse the site
+    const $ = Cheerio.load(HTML);
+    const URLs = [];
 
+    $("a").each((Index, Element) => {
+
+        let NewURL = $(Element).attr("href");
+
+        if (NewURL && NewURL.startsWith("/"))
+        {
+            // Convert relative URL to absolute URL
+            NewURL = new URL(NewURL, BaseURL).href;
+            URLs.push(NewURL);
+        }
+    });
+
+    return URLs;
+}
+
+async function ProcessPages(URLs, Depth)
+{
     for (let i = 0; i < URLs.length; i++)
     {
-        const HTML = GetHTMLFromURL(URLs[i]);
+        // Make sure you havent already processed this page
+        if (!ExploredPages.includes(URLs[i]))
+        {
+            AppendToArray(URLs[i], ExploredPages);
 
-        if (HTML)
-        {
-            const Information = ExtractInformationFromHTML(HTML);
-        }
-        
-        else
-        {
-            console.log("Error extracting HTML.");
+            // Throttle requests with a delay (0.5 - 1.5 secs) to avoid being blocked
+            Delay(Math.floor(Math.random() * 1000 + 500));
+
+            try
+            {
+                const HTML = await GetHTMLFromURL(URLs[i]);
+
+                ExtractInformationFromHTML(HTML);
+
+                // Process other pages on the site while limiting how deep it can explore
+                if (Depth < 5)
+                {
+                    const NewURLs = ExtractInteralLinkFromHtml(HTML, URLs[i]);
+                    await ProcessPages(NewURLs, Depth + 1);
+                }
+            }
+
+            catch (Error) {
+                console.error("Error processing HTML: ", Error);
+            }
         }
     }
 }
 
-const rl = readline.createInterface({
+function LogResults()
+{
+    console.log("\nEmails:", Emails);
+    console.log("\nPhone Numbers:", PhoneNumbers);
+    console.log("\nAddresses:", Addresses);
+    console.log("\nExplored Pages:", ExploredPages);
+}
+
+const ReadLineInterface = ReadLine.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 // Get email from user and process the sites home page
-rl.question("Email >>> ", (Input) => {
+ReadLineInterface.question("Email >>> ", async (Input) => {
 
     const CompanyDomain = Input.split("@")[1];
-    ProcessPages([`https://www.${CompanyDomain}`]);
-    rl.close();
+    await ProcessPages([`https://www.${CompanyDomain}`], 0);
+    LogResults();
+    ReadLineInterface.close();
 
 });
